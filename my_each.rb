9e767613e99e6c6742a3d57829ef7e52cd785078
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-16 19:42:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -9,17 +9,3 @@ def my_each(array)
   end
 end
 
-my_each(list) {|i| "Maybe #{i} is wasting his time."}
-
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(list) {|i| "Hello #{i} what's up?"}
